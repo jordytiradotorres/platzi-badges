e5640c873627b7cf34f6cbc84edeb67eba714f63
Jordy Tirado <jordytiradotorres20@gmail.com>
2020-02-01 11:47:18 -0500

    Updates

---
+++ precache-manifest.aeae7c535c895654b6f520d1f18d76b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfa887b8becc93bfb526",
+    "url": "/platzi-badges/static/css/main.f4e8ed4e.chunk.css"
+  },
+  {
+    "revision": "bfa887b8becc93bfb526",
+    "url": "/platzi-badges/static/js/main.bfa887b8.chunk.js"
+  },
+  {
+    "revision": "5688fd3e4cd38571a0cc",
+    "url": "/platzi-badges/static/js/runtime~main.5688fd3e.js"
+  },
+  {
+    "revision": "bc37fcf35c91015103bd",
+    "url": "/platzi-badges/static/css/2.264ce177.chunk.css"
+  },
+  {
+    "revision": "bc37fcf35c91015103bd",
+    "url": "/platzi-badges/static/js/2.bc37fcf3.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "77d062ba6b67ae481e383b763f8b39b9",
+    "url": "/platzi-badges/index.html"
+  }
+];
